0e241d0ce5c63c9f8daaafc1e8ea450fa2f9476e
solan <solan@FRAN>
2026-06-24 10:08:02 -0400

    veremos

--- SistemaCalificaciones/Program.cs
+++ SistemaCalificaciones/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins("http://localhost:5173",
             "https://sistema-calificaciones-front-12a3.vercel.app/",
-            "https://sistema-calificaciones-front-12a3-fk6lwiacj-mir-calificaciones.vercel.app/")
+            "https://sistema-calificaciones-front-12a3-fk6lwiacj-mir-calificaciones.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
