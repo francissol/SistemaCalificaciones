eeee1d77af2ace46a7136b2c53f62a01293a348c
solan <solan@FRAN>
2026-06-24 10:00:16 -0400

    prueba1

--- SistemaCalificaciones/Program.cs
+++ SistemaCalificaciones/Program.cs
@@ -21,7 +21,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("ReactPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173",
+            "https://sistema-calificaciones-front-12a3.vercel.app/")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
